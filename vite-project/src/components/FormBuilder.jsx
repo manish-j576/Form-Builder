a4cbe89eb196555502eea4fbe3bf4b1f49900c4a
Manish Joshi <manishjoshi705572@gmail.com>
2024-11-18 01:12:45 +0530

    fixed height issue in left section

--- vite-project/src/components/FormBuilder.jsx
+++ vite-project/src/components/FormBuilder.jsx
@@ -3,7 +3,7 @@ import { FieldLabel } from "./FieldLabel"
 import { FieldType } from "./FieldType"
 import {Heading} from "./Heading"
 export function FormBuilder(){
-    return <div style={{width:"45%",padding:"5px",background:"#1C1C1C",height:"100%",paddingBottom:"40px",paddingLeft:"30px",color:"white"}}>
+    return <div style={{width:"45%",padding:"5px",background:"#1C1C1C",height:"103%",paddingBottom:"40px",paddingLeft:"30px",color:"white"}}>
         <Heading data="Form Builder"></Heading>
         <FieldType></FieldType>
         <FieldLabel></FieldLabel>
